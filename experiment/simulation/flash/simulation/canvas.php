420865ee9f939dfeb44377004f4e1eddcfa8a874
saneeshpfrancis <saneeshpf@gmail.com>
2025-06-09 17:48:53 +0530

    simulator removed

--- experiment/simulation/flash/simulation/canvas.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" language="javascript">
-	var simPath="<?php getSimPath(); ?>";</script>
-<!-- Loading main file   --> 
-<script type="text/javascript" src="<?php getSimPath(); ?>js/simcontrols.js"></script>
-
-
--- experiment/simulation/flash/simulation/includes.php
+++
@@ -1,8 +0,0 @@
-<link  href="<?php getSimCss('simstyle.css'); ?>" rel="stylesheet" type="text/css" />
-<script src="<?php getSimPath(); ?>js/three.min.js" language="javascript"></script>
-<script src="<?php getSimPath(); ?>js/loaders/ColladaLoader.js"></script>
-<script src="<?php getSimPath(); ?>js/Detector.js"></script>
-<script src="<?php getSimPath(); ?>js/lib/prototype.js" type="text/javascript"></script>
-<script src="<?php getSimPath(); ?>js/src/scriptaculous.js" type="text/javascript"></script>
-
-
